416fdd0e355ee6a067fd035e47ff176bc60ec482
Daniel Gabriel <daniel@dangabriel.me>
2022-05-29 03:10:20 +0800

    Initial Commit

---
+++ CheckListApp/CheckListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckListAppApp.swift
+//  CheckListApp
+//
+//  Created by Daniel Gabriel on 5/29/22.
+//
+
+import SwiftUI
+
+@main
+struct CheckListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
